a200e5615f9252d240f9b1ff2acfadff2457a39e
Kelvin Christian <kelvin.santos@sptech.school>
2023-10-18 01:34:30 -0300

    funçoes de estilização e validação

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,65 @@ var content_logo = document.querySelector(".content-logo");
 var selector_field_position = document.querySelector(".selector-field");
 var cadastro_login = document.querySelector(".cadastroLogin");
 var title = document.querySelector(".title");
-var botao = document.querySelector(".button");
+
+campos_cadastro.style.display = "none"
+pointer.style.display = "none"
+// variáveis para cadastro e login
+
+var empresa_cadastro;
+var cnpj_cadastro;
+var email_cadastro;
+var senha_cadastro;
+
+var email_login;
+var senha_login;
 
 function cadastro() {
+    empresa_cadastro = inputEmpresa.value
+    cnpj_cadastro = inputCNPJ.value
+    email_cadastro = inputEmailCadastro.value
+    senha_cadastro = inputSenhaCadastro.value
+    point1.style.backgroundColor = ""
+    point2.style.backgroundColor = "white"
+    endereco()
+}
+
+function login() {
+    email_login = inputEmailLogin.value;
+    senha_login = inputSenhaLogin.value;
+
+    if (email_login == email_cadastro && senha_login == senha_cadastro) {
+        alert("Login bem sucedido")
+    } else {
+        alert("Cadastro não encontrado")
+    }
+}
+
+function endereco() {
+    title.innerHTML = "Endereço"
+    campos_cadastro.style.display = "none"
+    campos_endereco.style.display = "block"
+    cadastro_login.style.display = "none"
+}
+
+function cadastroEndereco() {
+    changeOption()
+    cadastro_login.style.display = "flex"
+    campos_endereco.style.display = "none"
+    point1.style.backgroundColor = ""
+    point2.style.backgroundColor = ""
+}
+
+function changeOption() {
+
     var valorcampo = cadastro_login.innerHTML;
     if (valorcampo == "Cadastrar") {
+
+        //alterar campos
+        campos_login.style.display = "none"
+        campos_cadastro.style.display = "block"
         //invertendo horizontalmente para tela de cadastro
+        frame.style.height = "70%"
         frame.style.borderColor = "rgba(102, 157, 158, 0.8)"
         frame.style.flexDirection = "row-reverse"
         //invertento posição do seletor para a direita
@@ -26,9 +79,17 @@ function cadastro() {
         //alterando cor do botao e mundando de Entrar para Cadastro
         forget.style.display = "none"
         title.innerHTML = "Cadastro"
-        botao.style.backgroundColor = "rgba(102, 157, 158, 0.8)"
+        title.style.margin = "3% 0 5% 0"
+        pointer.style.display = "flex"
+        point1.style.backgroundColor = "white"
+
     } else if(valorcampo == "Login") {
+
+        //alterar campos
+        campos_login.style.display = "block"
+        campos_cadastro.style.display = "none"
         //invertendo horizontalmente para tela de login
+        frame.style.height = "60%"
         frame.style.borderColor = "rgba(241, 189, 158, 0.4)"
         frame.style.flexDirection = ""
         //invertento posição do seletor para a esquerda
@@ -46,6 +107,8 @@ function cadastro() {
         //alterando cor do botao e mundando de Cadastro para Entrar
         forget.style.display = "block"
         title.innerHTML = "Login"
-        botao.style.backgroundColor = "rgba(241, 189, 158, 0.726)"
+        title.style.margin = "3% 0 15% 0"
+        pointer.style.display = "none"
+
     }
 }
