eb1c02b0b3de63e17d688be6aa67c5eb885e6d22
mhssouza <matheus.santos096@sptech.school>
2023-10-23 14:35:00 -0300

    Adicionando elementos na Dashboard e mexendo no menu

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -6,12 +6,14 @@ var botao_menu = document.querySelector(".button-menu")
 function menuClick() {
 	if (status_menu == 0) {
 		menu.classList.add("menu-active");
-		items_menu.style.marginRight = "20px"
+		items_menu.style.marginWidth = "20px"
+		items_menu.style.display = "block"
 		botao_menu.style.backgroundColor = "transparent"
 		status_menu = 1;
 	} else {
 		menu.classList.remove("menu-active");
-		items_menu.style.marginRight = "7px"
+		items_menu.style.marginWidth = "0px"
+		items_menu.style.display = "none"
 		botao_menu.style.backgroundColor = "rgba(0,0,0, 0.2)"
 		status_menu = 0;
 	}
