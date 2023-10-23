e1df85e42bf6b0ad5ee6028369cf38eeb3e63ba4
mhssouza <matheus.santos096@sptech.school>
2023-10-23 15:42:43 -0300

    Alterações e adições de novos elementos na Dashboard

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -7,13 +7,11 @@ function menuClick() {
 	if (status_menu == 0) {
 		menu.classList.add("menu-active");
 		items_menu.style.marginWidth = "20px"
-		items_menu.style.display = "block"
 		botao_menu.style.backgroundColor = "transparent"
 		status_menu = 1;
 	} else {
 		menu.classList.remove("menu-active");
 		items_menu.style.marginWidth = "0px"
-		items_menu.style.display = "none"
 		botao_menu.style.backgroundColor = "rgba(0,0,0, 0.2)"
 		status_menu = 0;
 	}
