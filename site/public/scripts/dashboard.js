0b3a66c24c8ab94cc3d069f8f66f58be1ce9e845
JuliaIFreitas <julia.freitas@sptech.school>
2023-11-27 20:04:58 -0300

    Alertas na dashboard adicionados

--- site/public/scripts/dashboard.js
+++ site/public/scripts/dashboard.js
@@ -16,3 +16,39 @@ function menuClick() {
 		status_menu = 0;
 	}
 }
+
+function gerarGraficos() { }
+
+function alertas() {
+	var temperatura = 21,
+		umidade = 0,
+		randTemp = 0,
+		randUmid = 0;
+
+	if (temperatura <= 14) {
+		// cardAlertaTemp.style.display = 'flex';
+		msgAlertaTemp.innerHTML = 'Existem armazéns com a temperatura <b>abaixo de 14°C</b>!'
+		cardAlertaTemp.style.borderColor = 'blue';
+	} else if (temperatura <= 17) {
+		// cardAlertaTemp.style.display = 'flex';
+		msgAlertaTemp.innerHTML = 'Existem armazéns com a temperatura <b>abaixo de 17°C</b>!'
+		cardAlertaTemp.style.borderColor = 'dodgerblue';
+	} else if (temperatura >= 20 && temperatura <= 23) {
+		msgAlertaTemp.innerHTML = 'Os armazéns estão na temperatura <b>ideal</b>!';
+		cardAlertaTemp.style.borderColor = 'limegreen';
+	} else if (temperatura <= 24) {
+		msgAlertaTemp.innerHTML = 'A temperatura dos armazéns estão <b>ACIMA de 24°C</b>!'
+		cardAlertaTemp.style.borderColor = 'orangered';
+	} else {
+		msgAlertaTemp.innerHTML = 'A temperatura dos armazéns está <b>CRÍTICA!!</b>'
+		cardAlertaTemp.style.borderColor = 'red';
+	}
+
+	if (umidade <= 55) {
+		msgAlertaUmid.innerHTML = 'Existem armazéns com a umidade <b>abaixo de 55%</b>!';
+		cardAlertaUmid.style.borderColor = 'blue';
+	} else if (umidade <= 58) {
+		msgAlertaUmid.innerHTML = 'Existem armazéns com a umidade <b>abaixo de 58%</b>!';
+		cardAlertaUmid.style.borderColor = 'dodgerblue';
+	}
+}
